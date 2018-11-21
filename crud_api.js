dd99c62d606ff8e80d496d468d4876a78ead8d2b
Chester C. Canites <chester.canites@gmail.com>
2018-11-21 22:06:23 +0800

    Connected to Database and Added GET Method endpoint

--- crud_api.js
+++ crud_api.js
@@ -1,13 +1,42 @@
 //load server using express
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
 
 // ----ROUTES---- //
 
-// GET METHOD
+// GET METHODS
+
+// get all inventory
 app.get("/inventory", (req, res) => {
-    console.log("Responding to rout route")
-    res.send("Hello")
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'inventory'
+    })
+    connection.query("SELECT * FROM items", (err, rows, fields) => {
+        console.log("Successfully fetch items")
+        res.json(rows)
+    })
+})
+
+// get specific inventory with id
+app.get("/inventory/:id", (req, res) => {
+    const id = req.params.id
+    const query = "SELECT * FROM items WHERE id = ?"
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'inventory'
+    })
+
+    connection.query(query,  [id], (err, rows, fields) => {
+        console.log("Successfully fetch item")
+        res.json(rows)
+    })
+    
 })
 
 // POST METHOD
@@ -15,7 +44,7 @@ app.get("/inventory", (req, res) => {
 // DELETE METHOD
 
 
-// localhost:8001
+// listen on localhost:8001
 app.listen(8001, () => {
     console.log("Server is up 8001...")
 })
