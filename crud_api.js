6850dbc191ba834419f9ce4fa951d9370def6701
Chester C. Canites <chester.canites@gmail.com>
2018-11-22 15:53:36 +0800

    installed nodemon and cors and added endpoint for put requests

--- crud_api.js
+++ crud_api.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
 // app.use(bodyParser.json())
 
@@ -18,22 +18,24 @@ const connection = mysql.createConnection({
     database: 'inventory'
 })
 
+const hostname = 'localhost'
+const port = 8001
+
 // ----ROUTES---- //
 
 // GET METHODS
 
 // get all inventory
-app.get("/inventory", (req, res) => {
+app.get("/api/inventory", (req, res) => {
 
     connection.query("SELECT * FROM items", (err, rows, fields) => {
-
         console.log("Successfully fetched items")
         res.json(rows)
     })
 })
 
 // get specific inventory with id
-app.get("/inventory/:id", (req, res) => {
+app.get("/api/inventory/:id", (req, res) => {
     const id = req.params.id
     const query = "SELECT * FROM items WHERE id = ?"
 
@@ -46,30 +48,58 @@ app.get("/inventory/:id", (req, res) => {
 
 // POST METHODS
 // Add item to inventory
-app.post("/create_item", (req, res) => {
+app.post("/api/inventory", (req, res) => {
 
     const newItem = req.body
-    const name = req.body.name
-    const qty = req.body.qty
-    const amount = req.body.amount
+    const id = newItem.id
+    const name = newItem.name
+    const qty = newItem.qty
+    const amount = newItem.amount
 
     console.log("Connected!");
-    const query = "INSERT INTO items (name, qty, amount) VALUES (?, ?, ?)";
+    const query = "INSERT INTO items (id, name, qty, amount) VALUES (?, ?, ?, ?)";
 
     connection.query(query, [name, qty, amount], (err, rows ,result) => {
         if (err) throw err;
         console.log("1 record inserted");
-        res.end("Post Successfully: \n" + JSON.stringify(newItem, null, 4))
+        res.end(JSON.stringify(newItem, null, 4))
     })
-
-
 })
 
 // PUT METHOD
+app.put("/api/inventory/:id", (req, res) => {
+    const itemId = req.params.id
+    var item = []
+    const query1 = "SELECT * FROM items WHERE id = ?"
+
+    connection.query(query1,  [itemId], (err, rows, fields) => {
+        console.log("Successfully fetched item")
+        for (var i of rows)
+            item.push(i)
+            console.log(item[0].id)
+
+            const keys = Object.keys(req.body)
+            console.log(req.body)
+
+            keys.forEach(key => {
+                console.log(item[0])
+                item[0][key] = req.body[key]
+            })
+
+            const query2 = "UPDATE items SET name = ?, qty = ?, amount = ? WHERE id = ?";
+            connection.query(query2, [item[0].name, item[0].qty, item[0].amount, item[0].id], (err, rows, fields) => {
+                if (err) throw err;
+                console.log("Record Updated");
+                res.end(JSON.stringify(item[0], null, 4))
+            })
+            // res.end(JSON.stringify(item[0], null, 4))
+    })
+})
+
 // DELETE METHOD
 
 
 // listen on localhost:8001
-app.listen(8001, () => {
-    console.log("Server is up 8001...")
+app.listen(port, hostname,() => {
+    console.log("Server is running at localhost:8001")
 })
